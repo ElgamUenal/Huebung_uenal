9adbda768be2c1414bde23ea1883a2010f8aab1c
elgam <elgam@LAPTOP-T0OE0NDJ>
2024-01-14 19:27:10 +0100

    Projektdateien hinzufügen.

---
+++ HÜ_14.01.04_ünal/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Geben Sie eine positive ganze Zahl ein: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+bool isPrime = IstPrimzahl(n);
+
+if (isPrime)
+{
+    Console.WriteLine($"{n} ist eine Primzahl.");
+}
+else
+{
+    Console.WriteLine($"{n} ist keine Primzahl.");
+}
+    
+
+    static bool IstPrimzahl(int num)
+{
+
+    if (num <= 1)
+    {
+        return false;
+    }
+
+
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            return false; // Die Zahl ist durch i teilbar, also keine Primzahl
+        }
+    }
+
+    return true;
+}
